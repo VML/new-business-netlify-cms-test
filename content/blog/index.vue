16ff43a72d311c523b4f3e5cc1b18856c0b756fc
Terrence Chambers <Terrence.Chambers@vmlyr.com>
2020-08-20 20:37:26 -0500

    updated index blog page

---
+++ content/blog/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+    </li>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const posts = await $content('blog').fetch();
+
+    return {
+      posts
+    }
+  }
+}
+</script>
